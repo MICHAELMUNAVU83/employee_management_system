d2272ed9ccc3a8284a1c52d2f18fb32a9d07c112
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-07-30 17:54:16 +0300

    Added

---
+++ lib/employee_management_system_web/live/submission_live/form_component.ex
@@ -0,0 +1,108 @@
+defmodule EmployeeManagementSystemWeb.SubmissionLive.FormComponent do
+  use EmployeeManagementSystemWeb, :live_component
+
+  alias EmployeeManagementSystem.Submissions
+
+  @impl true
+  def update(%{submission: submission} = assigns, socket) do
+    changeset = Submissions.change_submission(submission)
+
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign(:format_selected, "")
+     |> assign(:changeset, changeset)
+     |> assign(:uploaded_files, [])
+     |> allow_upload(:image, accept: ~w(.jpg .png .jpeg), max_entries: 1)
+     |> allow_upload(:pdf, accept: ~w(.pdf), max_entries: 1)}
+  end
+
+  @impl true
+  def handle_event("validate", %{"submission" => submission_params}, socket) do
+    changeset =
+      socket.assigns.submission
+      |> Submissions.change_submission(submission_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply,
+     socket
+     |> assign(:changeset, changeset)
+     |> assign(:format_selected, submission_params["type"])}
+  end
+
+  def handle_event("save", %{"submission" => submission_params}, socket) do
+    case socket.assigns.format_selected do
+      "image" ->
+        uploaded_files =
+          consume_uploaded_entries(socket, :image, fn %{path: path}, _entry ->
+            dest =
+              Path.join([
+                :code.priv_dir(:employee_management_system),
+                "static",
+                "uploads",
+                Path.basename(path)
+              ])
+
+            File.cp!(path, dest)
+            {:ok, "/uploads/" <> Path.basename(dest)}
+          end)
+
+        {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
+
+        new_submission_params = Map.put(submission_params, "image", List.first(uploaded_files))
+
+        save_submission(socket, socket.assigns.action, new_submission_params)
+
+      "pdf" ->
+        uploaded_files =
+          consume_uploaded_entries(socket, :pdf, fn %{path: path}, _entry ->
+            dest =
+              Path.join([
+                :code.priv_dir(:employee_management_system),
+                "static",
+                "uploads",
+                Path.basename(path)
+              ])
+
+            File.cp!(path, dest)
+            {:ok, "/uploads/" <> Path.basename(dest)}
+          end)
+
+        {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
+
+        new_submission_params = Map.put(submission_params, "pdf", List.first(uploaded_files))
+        save_submission(socket, socket.assigns.action, new_submission_params)
+
+      _ ->
+        save_submission(socket, socket.assigns.action, submission_params)
+    end
+  end
+
+  defp save_submission(socket, :edit_submission, submission_params) do
+    IO.inspect(submission_params)
+
+    case Submissions.update_submission(socket.assigns.submission, submission_params) do
+      {:ok, _submission} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Submission updated successfully")
+         |> push_redirect(to: socket.assigns.return_to)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
+
+  defp save_submission(socket, :add_submission, submission_params) do
+    case Submissions.create_submission(submission_params) do
+      {:ok, _submission} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Submission created successfully")
+         |> push_redirect(to: socket.assigns.return_to)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+end
